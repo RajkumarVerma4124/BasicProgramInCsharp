95b302108b6b62e4692ccba89354654056bc6b04
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-11 22:52:19 +0530

    Basic Programs Added

---
+++ BasicPrograms/CheckPrograms.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{ 
+    /// <summary>
+    ///   Methods Declaration For Basic programs
+    /// </summary>
+    internal class CheckPrograms
+    {
+        //To check even or odd number
+        public static void EvenOrOdd(int num)
+        {
+            string EoOresult = num % 2 == 0 ? "The num "+num+" is Even Number" : "The num "+num+" is Odd Number";
+            Console.WriteLine(EoOresult);
+            Console.ReadLine();
+        }
+
+        //To check positive or negative number 
+        public static void PosOrNeg(int num)
+        {
+            string PoNresult = num < 0 ? "The num " + num + " is Negative Number" : num == 0 ? " The number is zero" : " The num "+num+" is Positive Number";
+            Console.WriteLine(PoNresult);
+            Console.ReadLine();
+        }
+
+       
+
+
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+
+    /// <summary>
+    ///   Basic Programs
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine("Enter a choice from below : ");
+                Console.Write("1: Check Even Or Odd"+
+                            "\n2: Check Positive Or Negative"
+                            
+                             );
+                Console.WriteLine();
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter a num to check even or odd : ");
+                        int num = int.Parse(Console.ReadLine());
+                        CheckPrograms.EvenOrOdd(num);
+                        break;
+                    case 2:
+                        Console.Write("Enter a num to check positive or negative : ");
+                        int chknum = int.Parse(Console.ReadLine());
+                        CheckPrograms.PosOrNeg(chknum);
+                        break;
+                    
+                    default:
+                        Environment.Exit(0);
+                        return;
+                }  
+            }
+        }
+    }
+}
